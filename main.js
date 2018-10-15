1983e21028de1f21c8da51a57aa686b213417a2d
meiyiming <meiyiminghuiji@gmail.com>
2018-10-15 08:10:11 +0000

    episode 10

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-Vue.component('message',{
-    props:['title','body'],
-
-    data() {
-        return {
-            isVisiable: true
-        }
-    },
-
+Vue.component('modal',{
     template:`
-    <article class="message" v-show="isVisiable">
-        <div class="message-header">
-            <p>{{ title }}</p>
-            <button class="delete" aria-label="delete" @click='isVisiable = false'></button>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot></slot>
+            </div>
         </div>
-        <div class="message-body">
-            {{ body }}   
-        </div>
-    </article>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
     `
 });
 
 new Vue({
-    el:'#root'
+    el:'#root',
+
+    data:{
+        showModal:false
+    }
 });
