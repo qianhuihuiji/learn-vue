e384f0c9ef775cf4a32b311660ba5bb02997ee94
meiyiming <meiyiminghuiji@gmail.com>
2018-10-18 13:35:44 +0800

    episode 15

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
-  Vue.component('modal',{
-    template:`
-    <div class="modal is-active">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">
-                    <slot name="title">
-                        Default Title
-                    </slot>
-                </p>
-                <button class="delete" aria-label="close"></button>
-            </header>
-            <section class="modal-card-body">
-                <slot>
-                    Default Content.
-                </slot>
-            </section>
-            <footer class="modal-card-foot">
-                <slot name="footer">
-                    <button class="button is-success">Okay</button>
-                </solt>
-            </footer>
-        </div>
-    </div>
-    `
+  Vue.component('process-view',{
+      data(){
+          return { processRate:50 }
+      }
 });
 
 new Vue({
