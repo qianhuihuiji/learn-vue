a51eaf381ab0c8ff36a6938fdaf193a730cf4db6
meiyiming <meiyiminghuiji@gmail.com>
2018-10-18 11:57:49 +0800

    episode 13

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event,data = null){
+        this.vue.$emit(event,data);
+    }
+
+    listen(event,callback){
+        this.vue.$on(event,callback);
+    }
+}
+
 Vue.component('coupon',{
     template:'<input placeholder="enter your coupon code" @blur="onCouponApplied">',
 
     methods: {
         onCouponApplied() {
-            this.$emit('applied');
+            Event.fire('applied');
         }
     }
 });
@@ -15,10 +29,8 @@ new Vue({
         couponApplied:false
     },
 
-    methods: {
-        onCouponApplied() {
-            this.couponApplied = true;
-        }
+    created(){
+        Event.listen('applied',() => alert('Handing it!'));
     }
 });
 
